02c32f08247075e1d6a16695a823086b2ec86d1b
LuCeHe <manucelotti@gmail.com>
2020-04-14 09:57:16 +0200

    reduced the number of packages needed in the requirements.txt

--- convenience_functions/utils.py
+++ convenience_functions/utils.py
@@ -31,143 +31,18 @@
 
 import logging
 import os
-import random
-import time
-from time import strftime, localtime
 
 import numpy as np
-import tensorflow as tf
 import yagmail
 from tqdm import tqdm
 
 logger = logging.getLogger('mylogger')
 
 
-def make_directories(time_string=None):
-    experiments_folder = "experiments"
-    if not os.path.isdir(experiments_folder):
-        os.mkdir(experiments_folder)
-
-    if time_string == None:
-        time_string = strftime("%Y-%m-%d-at-%H:%M:%S", localtime())
-
-    experiment_folder = experiments_folder + '/experiment-' + time_string + '/'
-
-    if not os.path.isdir(experiment_folder):
-        os.mkdir(experiment_folder)
-
-        # create folder to save new models trained
-    model_folder = experiment_folder + '/model/'
-    if not os.path.isdir(model_folder):
-        os.mkdir(model_folder)
-
-        # create folder to save TensorBoard
-    log_folder = experiment_folder + '/log/'
-    if not os.path.isdir(log_folder):
-        os.mkdir(log_folder)
-
-    return experiment_folder
-
-
-def plot_softmax_evolution(softmaxes_list, name='softmaxes'):
-    import matplotlib.pylab as plt
-
-    f = plt.figure()
-    index = range(len(softmaxes_list[0]))
-    for softmax in softmaxes_list:
-        plt.bar(index, softmax)
-
-    plt.xlabel('Token')
-    plt.ylabel('Probability')
-    plt.title('softmax evoluti\on during training')
-    plt.show()
-    f.savefig(name + ".pdf", bbox_inches='tight')
-
-
-def checkDuringTraining(generator_class, indices_sentences, encoder_model, decoder_model, batch_size, lat_dim):
-    # original sentences
-    sentences = generator_class.indicesToSentences(indices_sentences)
-
-    # reconstructed sentences
-    point = encoder_model.predict(indices_sentences)
-    indices_reconstructed, _ = decoder_model.predict(point)
-
-    sentences_reconstructed = generator_class.indicesToSentences(indices_reconstructed)
-
-    # generated sentences
-    noise = np.random.rand(batch_size, lat_dim)
-    indicess, softmaxes = decoder_model.predict(noise)
-    sentences_generated = generator_class.indicesToSentences(indicess)
-
-    from prettytable import PrettyTable
-
-    table = PrettyTable(['original', 'reconstructed', 'generated'])
-    for b, a, g in zip(sentences, sentences_reconstructed, sentences_generated):
-        table.add_row([b, a, g])
-    for column in table.field_names:
-        table.align[column] = "l"
-    print(table)
-
-    print('')
-    print('number unique generated sentences:   ', len(set(sentences_generated)))
-    print('')
-    print(softmaxes[0][0])
-    print('')
-
-    return softmaxes
-
-
-def timeStructured():
-    named_tuple = time.localtime()  # get struct_time
-    time_string = time.strftime("%Y-%m-%d-%H-%M-%S", named_tuple)
-    return time_string
-
-
-def collect_information():
-    f = open('./cout.txt', 'r')
-    i = 0
-
-    for line in f:
-        if '>> "           AriEL' in line:
-            print(i)
-            i = 0
-            print(line)
-            g = open(line[-11:-2] + '.txt', 'w')
-            g.write('\n\n')
-            g.write(line)
-            g.write('\n\n')
-
-        if 'cpu' in line or 'CPU' in line:
-            if not 'I tensorflow' in line:
-                i += 1
-                g.write(line)
-                g.write('\n')
-    print(i)
-
-
-def setReproducible(seed=0, disableGpuMemPrealloc=True):
-    # Fix the seed of all random number generator
-    random.seed(seed)
-    np.random.seed(seed)
-    if tf.__version__[0] == '2':
-        tf.random.set_seed(seed)
-    else:
-        tf.random.set_random_seed(seed)
-
-    config = tf.compat.v1.ConfigProto(
-        intra_op_parallelism_threads=1,
-        inter_op_parallelism_threads=1,
-        device_count={'CPU': 1})
-    if disableGpuMemPrealloc:
-        config.gpu_options.allow_growth = True
-    # K.clear_session()
-    # K.set_session(tf.Session(config=config))
-
-
 def email_results(
         folders_list=[],
         filepaths_list=[],
-        text = '',
+        text='',
         name_experiment='',
         receiver_emails=[]):
     if not isinstance(receiver_emails, list): receiver_emails = [receiver_emails]
@@ -202,30 +77,3 @@ def email_results(
                                                                                            '\n'.join(failed))]
         for email in receiver_emails:
             yag.send(to=email, contents=contents, subject=subject)
-
-
-def email_folder_content(folderpath, receiver_email=''):
-    random_string = ''.join([str(r) for r in np.random.choice(10, 4)])
-    subject = random_string + ' The Experiment is [DONE] !'
-
-    content = os.listdir(folderpath)
-    print('content of the folder:\n')
-    for dir in content:
-        print('  ', dir)
-
-    if input("\n\nare you sure? (y/n)") != "y":
-        exit()
-
-    yag = yagmail.SMTP('my.experiments.336@gmail.com', ':(1234abcd')
-    failed = []
-    for dir in tqdm(content):
-        try:
-            path = os.path.join(folderpath, dir)
-            contents = [path]
-            yag.send(to=receiver_email, contents=contents, subject=subject)
-        except:
-            failed.append(dir)
-
-    contents = ['among all the files\n\n{} \n\nthese failed to be sent: \n\n{}'.format('\n'.join(content),
-                                                                                       '\n'.join(failed))]
-    yag.send(to=receiver_email, contents=contents, subject=subject)
