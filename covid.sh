67681aef710cecbb2a2bcdde265e70304f7190e6
LuCeHe <manucelotti@gmail.com>
2020-05-26 17:27:10 +0200

    minor changes

--- covid.sh
+++ covid.sh
@@ -3,6 +3,8 @@
 #SBATCH --account=def-jrouat
 #SBATCH --mem 8G
 #SBATCH --cpus-per-task 2
+#SBATCH --mail-user=luca.celotti@usherbrooke.ca
+#SBATCH --mail-type=END
 module load python/3.6
 source ~/projects/def-jrouat/lucacehe/denv2/bin/activate
 cd ~/projects/def-jrouat/lucacehe/work/covid19_gpt2
