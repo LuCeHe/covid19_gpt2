8a58bb8afcb0a97570cdd65277128c4332d81475
LuCeHe <manucelotti@gmail.com>
2020-06-07 13:33:50 +0200

    ready for long loop on Compute Canada

--- covid.sh
+++ covid.sh
@@ -6,7 +6,6 @@
 #SBATCH --gres=gpu:p100:1
 #SBATCH --mail-user=luca.celotti@usherbrooke.ca
 #SBATCH --mail-type=END
-
 module load python/3.6
 source ~/projects/def-jrouat/lucacehe/denv2/bin/activate
 cd ~/projects/def-jrouat/lucacehe/work/covid19_gpt2
